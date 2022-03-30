e0df5806ca1a0241e3dc6b48a8056d39d18e2cd1
RamadanKher <ramadankher1@gmail.com>
2022-03-30 09:31:20 +0200

    last commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,69 @@
 let imges=Array.from(document.getElementsByClassName("img-item")) ;
 let lightBox= document.getElementById("lightBox");
- let bacground=document.querySelector(".item-slider")
- let imgIndex=0;
-for (let index = 0; index < imges.length; index++) {
-   imges[index].addEventListener("click",function (e) {
-     imgSrc= e.target.src
-   imgIndex=imges.indexOf(e.target)
-  console.log(imgIndex)
-     bacground.style.backgroundImage=`url(${imgSrc})`
-     lightBox.classList.replace("d-none", "d-flex");
-    })
+let colsIcon= document.getElementById("colsIcon");
+let lightBoxBakground= document.getElementById("lightBoxBakground");
+let btnNextSlide= document.getElementById("btnNextSlide");
+let btnPrevSlide= document.getElementById("btnPrevSlide");
+let curentIndex=0;
+for (let i = 0; i < imges.length; i++) {
+ imges[i].addEventListener("click",function (e) {
+  lightBox.classList.replace("d-none","d-flex")
+    let imgSrc = e.target.src
+  lightBoxBakground.style.backgroundImage=`url(${imgSrc})`
 
+  curentIndex= imges.indexOf(e.target) //index img 
+ })
+  
 }
+// this function  for colse closeSlider
+function closeSlider() {
+  lightBox.classList.replace("d-flex","d-none")
+}
+// for btn x (close )
+colsIcon.addEventListener("click", closeSlider) 
 
-document.getElementById("nextSlide").addEventListener("click", nextSlide)
+// this function for NextSlide
 function nextSlide() {
-  imgIndex++;
-  if (imgIndex==imges.length) {
-    imgIndex=0
+  curentIndex ++
+  if (curentIndex==imges.length) {
+    curentIndex=0
   }
-  let sourcImge=imges[imgIndex].getAttribute('src');
-  bacground.style.backgroundImage=`url(${sourcImge})`
-}
+ let imgSrc= imges[curentIndex].src
 
-document.getElementById('prevSlide').addEventListener("click",prevSlider)
-function prevSlider() {
-  imgIndex--;
- if (imgIndex==-1) {
-  imgIndex=imges.length-1
- }
-  let sourcImge =imges[imgIndex].getAttribute('src');
-  bacground.style.backgroundImage=`url(${sourcImge})`
+  lightBoxBakground.style.backgroundImage=`url(${imgSrc})`
+  
 }
+// next slide
+btnNextSlide.addEventListener("click",nextSlide);
 
+// this function for prevSlide
+function prevSlide() {
+  curentIndex--;
+  if (curentIndex<0) {
+    curentIndex=imges.length-1
+  }
+ let imgSrc= imges[curentIndex].src
+
+  lightBoxBakground.style.backgroundImage=`url(${imgSrc})`
+  
+}
+// prev slide
+btnPrevSlide.addEventListener("click",prevSlide);
  
-
-document.getElementById('clos-icon').addEventListener('click',closSlider)
-function closSlider() {
-  lightBox.classList.replace("d-flex","d-none");
-
-}
-document.addEventListener('keydown',function (e) {
-if (e.key=="ArrowRight") {
+// keyboard nextslide and prevslide and exit 
+document.addEventListener("keydown", (e) => {
+if (e.key=="Escape") {
+  closeSlider()
+} else if(e.key=="ArrowRight") {
   nextSlide()
-}
-else if(e.key=="ArrowLeft") {
-  prevSlider()
-}
-else if(e.key=="Escape"){
-  closSlider()
+} else if(e.key=="ArrowLeft") {
+  prevSlide()
 }
 })
-
-document.body.addEventListener("click",function (e) {
-  if (e.target==lightBox) {
-    closSlider()
-  }
-},true );
-
-
-
+// 
+lightBox.addEventListener("click",(e) => {
+ 
+  if (e.target!==lightBoxBakground&&e.target!==btnNextSlide &&e.target!==btnPrevSlide) {
+    closeSlider()
+  } 
+})
